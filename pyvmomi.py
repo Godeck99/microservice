253d6d4acbb64611a23db7ac7af32b34aca23338
Godeck99 <95668775+Godeck99@users.noreply.github.com>
2024-02-01 12:43:08 -0500

    Update pyvmomi.py

--- pyvmomi.py
+++ pyvmomi.py
@@ -2,20 +2,14 @@ from fastapi import FastAPI, HTTPException
 from pyVmomi import vim, vmodl
 from pyVim.connect import SmartConnect, Disconnect
 import ssl
-import sys
 
 app = FastAPI()
 
 # Function to connect to the vCenter with SSL verification
 def get_vcenter_connection(host, user, password):
     try:
-        # Create an SSL context to be used for the connection
-        # This context will enforce certificate verification
         ssl_context = ssl.create_default_context()
         ssl_context.verify_mode = ssl.CERT_REQUIRED
-        # If necessary, you can specify CA certificates here:
-        # ssl_context.load_verify_locations('path/to/ca_certificates')
-
         service_instance = SmartConnect(host=host, user=user, pwd=password, sslContext=ssl_context)
         return service_instance
     except vim.fault.InvalidLogin:
@@ -31,18 +25,23 @@ def get_vm_by_name(service_instance, vm_name):
     container = content.viewManager.CreateContainerView(content.rootFolder, [vim.VirtualMachine], True)
     for vm in container.view:
         if vm.name == vm_name:
-            return vm
+            return {"vm_name": vm.name, "vm_status": vm.runtime.powerState}
     return None
 
 @app.get("/vm/{vm_name}")
 async def list_vm(vm_name: str, host: str, user: str, password: str):
-    service_instance = get_vcenter_connection(host, user, password)
-    if service_instance:
-        vm = get_vm_by_name(service_instance, vm_name)
-        Disconnect(service_instance)
-        if vm:
-            return {"vm_name": vm.name, "vm_status": vm.runtime.powerState}
+    service_instance = None
+    try:
+        service_instance = get_vcenter_connection(host, user, password)
+        vm_details = get_vm_by_name(service_instance, vm_name)
+        if vm_details:
+            return vm_details
         else:
             raise HTTPException(status_code=404, detail="VM not found")
-    else:
-        raise HTTPException(status_code=500, detail="Could not connect to vCenter")
+    except HTTPException as http_exc:
+        # Re-raise the HTTPException to be handled by FastAPI
+        raise http_exc
+    finally:
+        # Ensure we always disconnect, even if the above code raises an exception
+        if service_instance:
+            Disconnect(service_instance)
