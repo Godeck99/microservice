04a21c081bdeaa0a74034275835a0fdb8e863835
Godeck99 <95668775+Godeck99@users.noreply.github.com>
2024-02-01 18:21:36 -0500

    Create config.py

---
+++ pyvmomi/config.py
@@ -0,0 +1,6 @@
+import os
+
+# Retrieve sensitive data from environment variables
+VCENTER_HOST = os.getenv("VCENTER_HOST")
+VCENTER_USER = os.getenv("VCENTER_USER")
+VCENTER_PASSWORD = os.getenv("VCENTER_PASSWORD")
