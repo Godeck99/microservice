56b81e0b7be8b13e89b31ccca200fcb45a665ad5
Godeck99 <95668775+Godeck99@users.noreply.github.com>
2024-02-02 10:25:33 -0500

    Delete pyvmomi directory

--- pyvmomi/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# Retrieve sensitive data from environment variables
-VCENTER_HOST = os.getenv("VCENTER_HOST")
-VCENTER_USER = os.getenv("VCENTER_USER")
-VCENTER_PASSWORD = os.getenv("VCENTER_PASSWORD")
